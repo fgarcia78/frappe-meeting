21768c08e9023a58b400bf9f20e61db07ba7cc3c
Fernando García <fergarcia@erpnext6dev.itik-sistemas.com>
2016-03-25 14:34:43 -0700

    Primer commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+version = '0.0.1'
+requirements = parse_requirements("requirements.txt", session="")
+
+setup(
+	name='meeting',
+	version=version,
+	description='Prepare agenda, invite users and record minutes of a meeting',
+	author='Itik Sistemas',
+	author_email='contacto@itik-sistemas.com.mx',
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=[str(ir.req) for ir in requirements],
+	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+)
